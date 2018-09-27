0b4beeb4dbad498e8546f4986675cfbc899f2447
Ulises Olivares <uolivares@gdl.cinvestav.mx>
2018-09-27 16:22:55 -0500

    Update run_mer_inf.sh

--- healty/run_mer_inf.sh
+++ healty/run_mer_inf.sh
@@ -10,4 +10,4 @@
 #SBATCH --job-name="s3-h-inf-merged"
 
 # cmsearch infernal output 
-srun parallel -j 1 'cmsearch --cpu 45 -o {1.}.log --tblout {1.}.infernalout --anytrunc --rfam -E 0.001 rfam/Rfam.cm {1}' ::: *merged*bwa_blat.fasta
+srun parallel -j 1 'cmsearch --cpu 45 -o {1.}.log --tblout {1.}.infernalout --anytrunc --rfam -E 0.001 Rfam.cm {1}' ::: *merged*bwa_blat.fasta
